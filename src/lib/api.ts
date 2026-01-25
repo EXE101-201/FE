8ef33b55761c7850ce166853872dc5cd74f429fc
anhduc1408 <anhduc140804@gmail.com>
2026-01-25 16:00:27 +0700

    change to deloy

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://be-gprj.onrender.com/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
